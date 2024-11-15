8bb88f7cdccfbe8bd57b167c1d807c35749a08b6
jiranon.khemklad@gmail.com <jiranon.khemklad@gmail.com>
2024-11-15 10:22:19 +0700

    new Template

--- public/function/main.js
+++ public/function/main.js
@@ -74,7 +74,7 @@ function ReadInit() {
 
         // loop require Body ************************************
         requests.body.paths[key].require.body.forEach((element, index,array) => {
-            // console.log("log require : " + JSON.stringify(array));
+             console.log("log require : " + JSON.stringify(index));
             obj.paths[key][method].requestBody.content['application/json'].schema.required.push(element.key)
 
                 switch (element.type) {
@@ -150,9 +150,11 @@ function ReadInit() {
 
         }
         function setObjectRequire(objectRequire, element) {
-            for (const key in objectRequire.example) {
+             
+            objectRequire.required = [...element.requireKey];
+            for (const key in element.example) {
                 console.log(key)
-                objectRequire.properties[key] = { type: "string" };
+                objectRequire.properties[key] = { type: typeof element.example[key] };
             }
             obj.paths[key][method].requestBody.content['application/json'].schema.properties[element.key] = objectRequire;
             obj.paths[key][method].requestBody.content['application/json'].example[element.key] = element.example
@@ -176,188 +178,204 @@ function ReadInit() {
 
         // delete  obj.paths[key][method].requestBody.content['application/json'].schema.required
 
-        // for (const item in requests.body.paths[key].request.header) {
-        //     const objectParamerter = {
-        //         in: "header",
-        //         name: item,
-        //         schema: {
-        //             type: typeof item
-        //         },
-        //         required: false,
-        //         description: item
-        //     }
-        //     obj.paths[key][method].parameters.push(objectParamerter)
-        // }
+        for (const item in requests.body.paths[key].request.header) {
+            const objectParamerter = {
+                in: "header",
+                name: item,
+                schema: {
+                    type: typeof item
+                },
+                required: false,
+                description: item
+            }
+            obj.paths[key][method].parameters.push(objectParamerter)
+        }
 
 
-        //     for (const item in requests.body.paths[key].request.body) {
-        //         const value = requests.body.paths[key].request.body[item];
-        //         let objectResponseBody = {};
+            for (const item in requests.body.paths[key].request.body) {
+                const value = requests.body.paths[key].request.body[item];
+                console.log("log item : ",item)
+                let objectResponseBody = {};
 
-        //         if (Array.isArray(value)) {
-        //             objectResponseBody = {
-        //                 type: "array",
-        //                 example: []
-        //             };
+                if (Array.isArray(value)) {
+                    objectResponseBody = {
+                        type: "array",
+                        example: []
+                    };
 
-        //             for (const element of value) {
-        //                 let formattedElement = {};
+                    for (const element of value) {
+                        let formattedElement = {};
 
-        //                 for (const key in element) {
-        //                     const formattedKey = key.includes("@") ? `'${key}'` : key;
-        //                     formattedElement[formattedKey] = element[key];
-        //                 }
+                        for (const key in element) {
+                            const formattedKey = key.includes("@") ? `'${key}'` : key;
+                            formattedElement[formattedKey] = element[key];
+                        }
 
-        //                 objectResponseBody.example.push(formattedElement);
-        //             }
-        //         }
-        //         else if (typeof value === "object" && value !== null) {
-        //             objectResponseBody = {
-        //                 type: "object",
-        //                 properties: {}
-        //             };
+                         objectResponseBody.example.push(formattedElement);
+                    }
+                    obj.paths[key][method].requestBody.content['application/json'].example[item] = objectResponseBody.example;
 
-        //             for (const subKey in value) {
-        //                 const formattedSubKey = subKey.includes("@") ? `'${subKey}'` : subKey;
+                }
+                else if (typeof value === "object" && value !== null) {
+                    
+                
 
-        //                 objectResponseBody.properties[formattedSubKey] = {
-        //                     type: typeof value[subKey],
-        //                     example: value[subKey]
-        //                 };
-        //             }
-        //         }
-        //         else {
-        //             objectResponseBody = {
-        //                 type: typeof value,
-        //                 example: value
-        //             };
-        //         }
+                    objectResponseBody = {
+                        type: "object",
+                        properties: {}
+                    };
 
-        //         obj.paths[key][method].requestBody.content['application/json'].schema.properties[item] = objectResponseBody;
-        //     // }
+                    for (const subKey in value) {
+                       
+                        const formattedSubKey = subKey.includes("@") ? `'${subKey}'` : subKey;
+                        console.log("format key : ",formattedSubKey )
+                        objectResponseBody.properties[item] = {
+                            type: typeof value[subKey],
+                            
+                        };
+                      
+                    }
+                    obj.paths[key][method].requestBody.content['application/json'].example[item] = value;
+                    console.log("log value",obj.paths[key][method].requestBody.content['application/json'].example)
 
+                }
+                else {
+                    objectResponseBody = {
+                        type: typeof value,
+                        example: value
+                    };
+                    obj.paths[key][method].requestBody.content['application/json'].example[item] = value;
+                }
+                const withOutExample = {
+                    type: typeof value
+                }
+              
+                obj.paths[key][method].requestBody.content['application/json'].schema.properties[item] = withOutExample;
+                // obj.paths[key][method].requestBody.content['application/json'].example[item] = objectResponseBody.example;
+                // console.log("log value 2",obj.paths[key][method].requestBody.content['application/json'].example)
 
-
-
-
-        //     // console.log("log res : " +  JSON.stringify(Object.keys(requests.body.paths[key].response).length))
-
-        //         for (const item in requests.body.paths[key].response) {
-
-        //             // console.log("item : check" + item)
-        //             const responseData = {
-
-        //                 description: requests.body.paths[key].response[item].description,
-        //                 headers: {
-
-        //                 },
-
-        //                 content: {
-        //                     "application/json": {
-        //                         schema: {
-        //                             type: "object",
-        //                             properties: {
-
-        //                             },
-        //                             required: []
-        //                         }
-        //                     }
-        //                 }
-        //             };
+            // }
 
 
 
-        //             const res = requests.body.paths[key].response[item]
-        //             if (Object.keys(res.require.header).length != 0) {
-        //                 for (const key in res.require.header) {
-
-        //                     const value = res.require.header[key];
-        //                     const objectHeader = {
-        //                         description: key,
-        //                         required: true,
-        //                         schema: {
-        //                             type: typeof value,
-        //                             example: value
-        //                         }
-
-        //                     }
-        //                     responseData.headers[key] = objectHeader
-        //                 }
-        //             }
-        //             else {
-        //                 // responseData.content['application/json'].schema.header[`'${key.toString()}'`] = objectBody
-        //             }
-        //             if(Object.keys(res.require.body).length != 0){
-        //             for (const key in res.require.body) {
-        //                 const value = res.require.body[key];
-
-        //                 const objectBody = {
-        //                     example: value
-        //                 };
-
-        //                 responseData.content['application/json'].schema.required.push(key)
-        //                 responseData.content['application/json'].schema.properties[`'${key.toString()}'`] = objectBody
-        //             }
-        //         }else{
-        //            delete responseData.content['application/json'].schema.required  
-        //         //    console.log("check log : " + Object.keys(res.require.body).length) 
-        //         }
-
-        //             for (const key in res.nonRquire.header) {
-        //                 const value = res.nonRquire.header[key];
-        //                 const objectHeader = {
-        //                     description: key,
-        //                     required: true,
-        //                     schema: {
-        //                         type: typeof value,
-        //                         example: value
-        //                     }
-
-        //                 }
-        //                 responseData.headers[key] = objectHeader
-        //             }
-
-        //             for (const key in res.nonRquire.body) {
-        //                 const value = res.nonRquire.body[key];
-        //                 // console.log("check value : " + JSON.stringify(value) + " = " + JSON.stringify(typeof value));
-
-        //                 let objectBody;
-
-        //                 if (Array.isArray(value)) {
-
-        //                     objectBody = {
-        //                         example: value.map(item => checkAndAct(item))
-        //                     };
-        //                 } else if (typeof value === "object" && value !== null) {
-        //                     objectBody = {
-        //                         example: checkAndAct(value)
-        //                     };
-        //                 } else if (typeof value === "string") {
-        //                     objectBody = {
-        //                         example: checkAndAct(value)
-        //                     };
-        //                 } else {
-        //                     objectBody = {
-        //                         example: value
-        //                     };
-        //                 }
-
-        //                 responseData.content['application/json'].schema.properties[key] = objectBody;
-        //             }
-
-        //             try {
-        //                 obj.paths[key][method].responses[item] = responseData;
-        //             } catch (error) {
-        //                 console.log("catch : ", error);
-        //             }
 
 
-        //         }
+            // console.log("log res : " +  JSON.stringify(Object.keys(requests.body.paths[key].response).length))
 
+        
 
-
-        // };
-
+        };
+        for (const item in requests.body.paths[key].response) {
+            console.log("in case response")
+                
+                const responseData = {
+    
+                    description: requests.body.paths[key].response[item].description,
+                    headers: {
+    
+                    },
+    
+                    content: {
+                        "application/json": {
+                            schema: {
+                                type: "object",
+                                properties: {
+    
+                                },
+                                required: []
+                            }
+                        }
+                    }
+                };
+    
+    
+    
+                const res = requests.body.paths[key].response[item]
+                if (Object.keys(res.require.header).length != 0) {
+                    for (const key in res.require.header) {
+    
+                        const value = res.require.header[key];
+                        const objectHeader = {
+                            description: key,
+                            required: true,
+                            schema: {
+                                type: typeof value,
+                                example: value
+                            }
+    
+                        }
+                        responseData.headers[key] = objectHeader
+                    }
+                }
+                else {
+                    // responseData.content['application/json'].schema.header[`'${key.toString()}'`] = objectBody
+                }
+                if(Object.keys(res.require.body).length != 0){
+                for (const key in res.require.body) {
+                    const value = res.require.body[key];
+    
+                    const objectBody = {
+                        example: value
+                    };
+    
+                    responseData.content['application/json'].schema.required.push(key)
+                    responseData.content['application/json'].schema.properties[`'${key.toString()}'`] = objectBody
+                }
+            }else{
+               delete responseData.content['application/json'].schema.required  
+          
+            }
+    
+                for (const key in res.nonRquire.header) {
+                    const value = res.nonRquire.header[key];
+                    const objectHeader = {
+                        description: key,
+                        required: true,
+                        schema: {
+                            type: typeof value,
+                            example: value
+                        }
+    
+                    }
+                    responseData.headers[key] = objectHeader
+                }
+    
+                for (const key in res.nonRquire.body) {
+                    const value = res.nonRquire.body[key];
+                    // console.log("check value : " + JSON.stringify(value) + " = " + JSON.stringify(typeof value));
+    
+                    let objectBody;
+    
+                    if (Array.isArray(value)) {
+    
+                        objectBody = {
+                            example: value.map(item => checkAndAct(item))
+                        };
+                    } else if (typeof value === "object" && value !== null) {
+                        objectBody = {
+                            example: checkAndAct(value)
+                        };
+                    } else if (typeof value === "string") {
+                        objectBody = {
+                            example: checkAndAct(value)
+                        };
+                    } else {
+                        objectBody = {
+                            example: value
+                        };
+                    }
+    
+                    responseData.content['application/json'].schema.properties[key] = objectBody;
+                }
+    
+                try {
+                    obj.paths[key][method].responses[item] = responseData;
+                } catch (error) {
+                    console.log("catch : ", error);
+                }
+    
+    
+            }
 
 
         const yamlData = YAML.stringify(obj);
