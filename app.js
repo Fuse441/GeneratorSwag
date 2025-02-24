232d99466a482eac91a0b52897c562ef48739961
jirak443 <jiranon.k@entronica.in.th>
2025-02-24 15:03:23 +0700

    update cors

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const app = express();
 const { ErrorHandler } = require("./middleware/error.handler");
-
+const core = require('cors')
 app.use(express.json());
 const port = 25565;
 
 app.use(express.static("public"));
-
+app.use(core())
 const mainRouter = require("./routes/main");
 
 app.use("/", mainRouter);
